26caacb66663d596e7aa20b360120d3ae236b9d4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "resource_groups" {
-  description = "All resource_group resources"
-  value       = azurerm_resource_group.resource_groups
-}
 output "resource_groups_location" {
-  description = "List of location values across all resource_groups"
-  value       = [for k, v in azurerm_resource_group.resource_groups : v.location]
+  description = "Map of location values across all resource_groups, keyed the same as var.resource_groups"
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.location }
 }
 output "resource_groups_managed_by" {
-  description = "List of managed_by values across all resource_groups"
-  value       = [for k, v in azurerm_resource_group.resource_groups : v.managed_by]
+  description = "Map of managed_by values across all resource_groups, keyed the same as var.resource_groups"
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.managed_by }
 }
 output "resource_groups_name" {
-  description = "List of name values across all resource_groups"
-  value       = [for k, v in azurerm_resource_group.resource_groups : v.name]
+  description = "Map of name values across all resource_groups, keyed the same as var.resource_groups"
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.name }
 }
 output "resource_groups_tags" {
-  description = "List of tags values across all resource_groups"
-  value       = [for k, v in azurerm_resource_group.resource_groups : v.tags]
+  description = "Map of tags values across all resource_groups, keyed the same as var.resource_groups"
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.tags }
 }
 
