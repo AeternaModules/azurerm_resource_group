52c402574aec0c4eb44099cd0ac6b020ddae1725
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:29 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "resource_groups_id" {
+  description = "Map of id values across all resource_groups, keyed the same as var.resource_groups"
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.id }
+}
 output "resource_groups_location" {
   description = "Map of location values across all resource_groups, keyed the same as var.resource_groups"
   value       = { for k, v in azurerm_resource_group.resource_groups : k => v.location }
