cc7ccfe097faa499977f16f7d35cc3f2b1f108c2
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "resource_groups_id" {
   description = "Map of id values across all resource_groups, keyed the same as var.resource_groups"
-  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.id }
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "resource_groups_location" {
   description = "Map of location values across all resource_groups, keyed the same as var.resource_groups"
-  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.location }
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.location if v.location != null && length(v.location) > 0 }
 }
 output "resource_groups_managed_by" {
   description = "Map of managed_by values across all resource_groups, keyed the same as var.resource_groups"
-  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.managed_by }
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.managed_by if v.managed_by != null && length(v.managed_by) > 0 }
 }
 output "resource_groups_name" {
   description = "Map of name values across all resource_groups, keyed the same as var.resource_groups"
-  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.name }
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "resource_groups_tags" {
   description = "Map of tags values across all resource_groups, keyed the same as var.resource_groups"
-  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.tags }
+  value       = { for k, v in azurerm_resource_group.resource_groups : k => v.tags if v.tags != null && length(v.tags) > 0 }
 }
 
